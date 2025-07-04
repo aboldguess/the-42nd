52d4a966976eb72e4cca7b38ef587192e096a624
aboldguess <phil.catton@gmail.com>
2025-07-04 12:32:25 +0100

    Move create sidequest button above table

--- client/src/pages/SideQuestStatusPage.js
+++ client/src/pages/SideQuestStatusPage.js
@@ -4,12 +4,18 @@ import ItemTablePage from './ItemTablePage';
 export default function SideQuestStatusPage() {
   return (
     <div>
-      <ItemTablePage type="sidequest" titlePrefix="Side Quests" />
+      {/*
+        Provide quick access to start a new quest. Placing the button
+        above the table keeps it visible even when the quest list grows
+        long and requires scrolling.
+      */}
       <div style={{ margin: '1rem' }}>
         <a href="/sidequests/create">
           <button>Create New Sidequest</button>
         </a>
       </div>
+      {/* Table showing all side quests and their completion status */}
+      <ItemTablePage type="sidequest" titlePrefix="Side Quests" />
     </div>
   );
 }
